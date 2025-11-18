8500d9ca3924761c872273c6d8a864ded6b96b89
JonasEmerick <jonasemerick@icloud.com>
2025-11-18 02:14:52 -0300

    Finalização da integração com o banco de dados fazendo as ações necessárias para a tela de login, registro e controle de tasks do site

--- app.js
+++ app.js
@@ -3,192 +3,167 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const bcrypt = require("bcrypt");
+const {getPool} = require('./connection')
 
 
-//Funcão de validação de e-mail no padrao xxxxx@xxxx.com
-function validateEmail(email){
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-}
 
 //Middleware para ler JSON no corpo da requisição
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//Banco de dados:
-/* const { Pool } = require("pg");
 
-const pool = new Pool({
-    user: "jonasemerick@icloud.com",
-    host: "",
-    database: "",
-    password: "",
-    port: 5432,
-    ssl: true
+//Realizar consultas no SQL
 
-}) */
+async function execSQLQuery(sqlQry) {
+    const pool = await getPool();
+    const { recordset } = await pool.request().query(sqlQry);
+    return recordset;
+}
 
-const userList = [];
 
-//Forma get de pegar login
-//app.get('/login', function(req, res) {
-    //const usuario = req.query.usuario;
-    //const pass = req.query.pass;
+/* app.put('/register/:user_id', (req, res)=>{
+    const id = req.params.user_id;
+})
+ */
 
-//Forma post(mais segura) de pegar login
-app.post("/login", async(req, res) => {
-    const {email, pass, id} = req.body;
 
-//Verificação de dados de login:
+//Consultar todos os logins
 
-//se ambos estiverem preenchidos fazer um select para trazer o usuário do banco de dados
-
-//Verificar se os dados abaixo estão vazios:
-const login = userList.find(user => user.email === email);
-
-    if(!login || !pass){
-        return res.status(400).json({error: 'Login ou senha inválidos!'})
+app.get('/login', async (req, res) => {
+    const aUsers = await execSQLQuery('SELECT * FROM users');
+    
+    if (!aUsers.length) {
+        res.status(400).json({ error: 'Não existe usuários' });
     }
-
-    const criptoPass = await bcrypt.compare(pass, login.pass);
-
-    if(login && criptoPass){
-        return res.status(200).json({message: 'Login realizado com sucesso!'});
-
-    }
-        return res.status(400).json({error: 'Login ou senha inválidos!'});
+    
+    return res.status(200).json(aUsers);
 });
 
-app.post("/register", async(req, res) => {
-    const {email, pass, user_id} = req.body;
+//Adicionando um novo login
 
+app.post('/register', async (req, res) => {
+    const { email, pass } = req.body;
 
-// Verificação de dados de registro:
-
-    if(!email || !pass){
-        return res.status(400).json({error: 'É necessaŕio preencher todos os campos!'});
-
-    }else if(pass.length < 6){
-         return res.status(400).json({error: 'A pass não pode ter menos de 6 digitos!'});
-
-    }else if(!validateEmail(email)){  
-        return res.status(400).json({error: 'E-mail invalido!'});  
-
+    if (!email && !pass) {
+        return res.status(400).json({ error: 'É necessário preencher todos os campos!' });
     }
 
-    //Verificação se o usuário existe no banco de dados:
-    const userExist = userList.find(user => user.user_id === user_id && user.email === email);
+    const loginExist = await execSQLQuery('SELECT * FROM users WHERE email=' + email);
 
-    if (userExist){
-        return res.status(200).json({error: 'Usuário já cadastrado!'});
+    if (loginExist) {
+        return res.status(400).json({ error: 'E-mail já cadastrado!' });
     }
 
-    //Incluir usuário no banco de dados:
-    const criptoPass = await bcrypt.hash(pass,10);
-    userList.push({user_id ,email, pass: criptoPass});
-    return res.status(200).json({message: 'Cadastro realizado!'});
+    await execSQLQuery(`INSERT INTO users(email, pass) VALUES('${email}','${pass}')`)
+    res.status(200).json({ message: 'Dados de login inseridos!' });
+});
 
+
+//Consultar login para acesso
+
+app.get('/login/:id', async (req, res)=>{
+    const id = req.params.id;
+    const {email, pass} = req.body;
+    if(!id){
+        return res.status(400).json({error: 'ID não informado.'});
+    }
+    const idExist = await execSQLQuery('SELECT * FROM users WHERE id='+id);
+    if(!idExist.length){
+        return res.status(400).json({error: 'ID não existe'});
+    }
+    const userExist = await execSQLQuery(`SELECT * FROM user WHERE id=${id} AND email=${email} AND pass=${pass}`);
+    if(!userExist.length){
+        return res.status(400).json({error:'Login ou senha inválidos!'})
+    }
+    return res.status(200).json({message: 'Login realizado!'});
+});
+
+//Trocar de senha de login
+
+app.put('/login/:id', async (req, res)=>{
+   const id = req.params;
+   const {email, newPass} = req.body;
+   if(!id){
+    return res.status(400).json({error: 'Id não informado.'});
+   }
+   const idExist = await execSQLQuery(`SELECT * FROM users WHERE id=${id}`);
+   if(!idExist.length){
+    return res.status(400).json({error: 'Id não encontrado!'});
+   }
+   const userExist = await execSQLQuery(`SELECT * FROM users WHERE id=${id} AND email=${email}`);
+   if(!userExist.length){
+    return res.status(400).json({error: 'Email não encontrado!'})
+   }
+   await execSQLQuery(`UPDATE users SET pass= ${newPass} WHERE id=${id}`);
+   return res.status(200).json({message: 'Senha atualizada!'});
+});
+
+
+//Consultar tasks do Usuario
+app.get('/tasks', async(req, res) => {
+    const {user_id} = req.query;
+    const aTasks = await execSQLQuery(`SELECT * FROM tasks WHERE user_id=${user_id}`);
+    if(!aTasks){
+        return res.status(400).json({error:'Id sem tasks!'});
+    }
+    return res.status(200).json(aTasks);
+    
+});
+
+/* //"id": 1,
+    "user_id": 1,
+    "name": "Criar layout da página inicial",
+    "priority": "Alta",
+    "status": "Pendente",
+    "created_at": "2025-11-14T03:10:53.555Z",
+    "completed_at": null */
+
+app.post('/tasks', async(req, res) => {
+    const {user_id,name, priority, status, created_at, completed_at}= req.body;
+
+    if (!user_id){
+        return res.status(400).json({error: 'Id user não informado!'})
+    }
+    await execSQLQuery(`INSERT INTO tasks(user_id, name, priority, status, created_at, completed_at) VALUES (${user_id},'${name}',
+        '${priority}', '${status}', '${created_at}', '${completed_at}')`);
+    res.status(200).json({message:'Tasks incluida com sucesso'});
     
 
+
 });
 
-//Alterar pass
-app.put("/register", async(req, res) => {
-    const {email, pass, id} = req.body;
-    const aUser = userList.findIndex(user => user.id === id);
+//Alterar dados da task
 
-    if(aUser){
-        const criptoPass = await bcrypt.hash(pass,10);
-        userList[aUser].pass = criptoPass;
-        return res.status(200).json({message: 'Alteração de senha realizada!'});
-
+app.put('/tasks/:id', async(req, res)=>{
+    const taskId = req.params.id;
+    if(!taskId){
+        return res.status(400).json({error: 'Id não informado!'})
     }
-
-});
-
-app.get("/user", function(req, res) {
-  return res.json(userList);
-});
-
-//Banco de dados de Tasks
-
-const taskList = [];
-
-//Inclusão no banco da tasks
-
-app.post("/user/:user_id/tasks/" , function(req , res) {
-    const user_id = req.params.user_id;
-    const {task_id, title, description, status} = req.body;
-    const taskExist = taskList.find(tasks => tasks.task_id === task_id && tasks.user_id === user_id);
-    if(!taskExist){
-        if(!task_id || !title || !description || !status){
-            return res.status(400).json({error: 'É necessário preencher todos os campos!'})
-
-
-        }
-            taskList.push({user_id ,task_id, title, description, status});
-            return res.status(200).json({message: 'Task cadastrada!'});
+    const {user_id, name, priority, status, created_at, completed_at} = req.body;
+    const taskExist = await execSQLQuery(`SELECT * FROM [tasks] WHERE user_id=${user_id} AND id=${taskId}`);
+    console.log(`SELECT * FROM [tasks] WHERE user_id=${user_id} AND id=${taskId}`)
+    if(!taskExist.length){
+        return res.status(400).json({error:'Task não encontrada!'});
     }
-    res.status(400).json({error: 'ID já cadastrado!'})
+    await execSQLQuery(`UPDATE [tasks] SET name='${name}', priority='${priority}', 
+        status='${status}', created_at='${created_at}', completed_at='${completed_at}' WHERE id=${taskId} AND user_id=${user_id}`);   
+    
+    return res.status(200).json({message: 'Dados atualizados!'});
 });
-//Alterar o Status da tasks
-app.put("/user/:user_id/tasks/:task_id", function(req, res) {
-    const {id, title, description, status} = req.body;
-    const userId = req.params.user_id;
-    const userIdExist = userList.firndIndex(user => user.id === userId)
-    const aTaskList = taskList.findIndex(tasks => tasks.id === id);
-    //Alterar o status da tasks:
-    if(userIdExist){
-    if(aTaskList){
 
-        taskList[aTaskList].status = status;
-        taskList[aTaskList].title = title;
-        taskList[aTaskList].description = description;
-        return res.status(200).json({message: 'Dados da tasks alterados!'});
+
+//Deletar Tasks
+
+app.delete('/tasks/:id', async(req, res)=>{
+    const taskId = req.params.id;
+    if(!taskId){
+        return res.status(400).json({error: 'Id não inserido'});
     }
-    return res.status(400).json({error: 'Task não encontrada!'});
-    }
-    return res.status(200).json({error: 'Id não localizado!'})
-
-});
-
-//Deletar uma tasks
-app.delete("/user/:user_id/tasks/:task_id", function(req, res) {
-    const {userId, taskId} = req.params;
-    const aTaskList = taskList.findIndex(tasks => tasks.id === taskId);
-    if(aTaskList){
-        const deleteTask = taskList.splice(aTaskList, 1);
-        return res.status(200).json({message: `A task com id  ${id} foi excluído!`});
-
-    }
-    return res.status(400).json({error: 'Task não encontrada!'});
-
-
-});
-
-//Pegar tasks
-app.get("/user/:user_id/tasks/:task_id", function(req, res){
-    const {user_id, task_id} = req.params;
-    const taskIndex = taskList.findIndex(task => task.user_id === user_id && task.task_id === task_id);
-
-    if(taskIndex>=0){
-        return res.status(200).json ({message: taskList[taskIndex]});
-    }
-    return res.status(400).json({error: 'Task não localizada!'});
-
-});
-
-
-//Listar as tasks
-app.get("/tasks", function (req, res) {
-   return res.json (taskList);
-
-});
-
-app.get("")
+    const user_id = req.body.user_id;
+    await execSQLQuery(`DELETE [tasks] WHERE user_id=${user_id} AND id=${taskId}`);
+    return res.status(200).json({response: 'Task deletada!'});
+})
 
 
 
 http.createServer(app).listen(port, () => console.log("Servidor rodando local na porta 3000"));
-
-
--- connection.js
+++ connection.js
@@ -1,29 +1,29 @@
-const sql = require("mssql");
-const dotenv = require("dotenv");
+const sql = require('mssql');
+const dotenv = require('dotenv')
 
-
-dotenv.config();
+dotenv.config()
 
 const config = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    server: process.env.DB_SERVER,
-    database : process.env.DB_NAME,
-    port: process.env.DP_PORT,
-    options: {
-        encrypt: true,
-        trustServerCertificate: false
-    }
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_SERVER,  
+  database: process.env.DB_NAME,    
+  port: 1433,
+  options: {
+    encrypt: true,
+    trustServerCertificate: false
+  }
+};
+
+async function getPool() {
+
+  try {
+    const pool = await sql.connect(config)
+    return pool
+  } catch (error) {
+    console.log('Erro conexão DB:', error)
+  }
+  
 }
 
-async function getPool(){
-    try{
-        const pool = await sql.connect(config);
-        return pool;
-
-    }catch(error){
-        console.log("Erro de conexão DB", error);
-    }
-}
-
-module.exports = { getPool, sql};
+module.exports = { getPool }
